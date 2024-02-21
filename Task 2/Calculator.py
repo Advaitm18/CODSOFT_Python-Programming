75f33fe726f6fab0ca7647fc0f867a9ef0885f10
Advait_Mane <advaitm18@gmail.com>
2024-02-21 17:51:58 +0530

    Update Calculator.py

--- Task 2/Calculator.py
+++ Task 2/Calculator.py
@@ -1,3 +1,5 @@
+import termtables
+
 print('''
       _____      _            _       _          
      / ____|    | |          | |     | |            
@@ -5,28 +7,34 @@ print('''
     | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__|
     | |___| (_| | | (__| |_| | | (_| | || (_) | |   
      \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
-    \n''')
+\n''')
 
-def add(x,y):
+def add(x,y):                  # This function performs Addition 
     return x + y
 
-def subtract(x,y):
+def subtract(x,y):             # This function performs Subtraction 
     return x - y
 
-def multiply(x,y):
+def multiply(x,y):             # This function performs Multiplication 
     return x * y
 
-def exponent(x,y):
+def exponent(x,y):             # This function performs Exponentiation 
     return x ** y
 
-def divide(x,y):
+def divide(x,y):               # This function performs Float Division
     return x / y
 
-def floordivide(x,y):
+def floordivide(x,y):          # This function performs Floor Division
     return x // y
 
+def tabledisplay(op,result):   # This function display the Operational Output
+    my_table=[
+        [f"{op}" , f"{result}"]
+    ]
+    headers= ["Arithmetic Operation","Operational Output"]
+    termtables.print(my_table,header=headers,padding=(0,1),alignment="cc")
+    
 while True:
-
     print("\nSelect Arithmetic Operation: ")
     print("1. Addition")
     print("2. Subtraction")
@@ -34,26 +42,44 @@ while True:
     print("4. Exponentiation")
     print("5. Division (Float)")
     print("6. Division (Floor)")
-    print("7. Exit")
 
     option = int(input("\n---> "))
     
     num1 = int(input("\nEnter first number: "))
     num2 = int(input("\nEnter second number: "))
-    
+   
     if option == 1:
-        print(f"{num1} + {num2} = {add(num1,num2)}")
+        op = f"{num1} + {num2}"
+        result = f"{add(num1,num2)}"
+        tabledisplay(op,result)
     elif option == 2:
-        print(f"{num1} - {num2} = {subtract(num1,num2)}")
+        op = f"{num1} - {num2}"
+        result = f"{subtract(num1,num2)}"
+        tabledisplay(op,result)
     elif option == 3:
-        print(f"{num1} * {num2} = {multiply(num1,num2)}")
+        op = f"{num1} * {num2}"
+        result = f"{multiply(num1,num2)}"
+        tabledisplay(op,result)
     elif option == 4:
-        print(f"{num1} ** {num2} = {exponent(num1,num2)}")
+        op = f"{num1} ** {num2}"
+        result = f"{exponent(num1,num2)}"
+        tabledisplay(op,result)
     elif option == 5:
-        print(f"{num1} / {num2} = {divide(num1,num2)}")   
+        op = f"{num1} / {num2}"
+        result = f"{divide(num1,num2)}"
+        tabledisplay(op,result)
     elif option == 6:
-        print(f"{num1} // {num2} = {floordivide(num1,num2)}")
-    elif option == 7:
+        op = f"{num1} // {num2}"
+        result = f"{floordivide(num1,num2)}"
+        tabledisplay(op,result)
+    else:
+       print("Invalid input!! ")
+          
+    print("Do you want to continue y/n ??")
+    choice= input("---> ").upper()
+    if choice == "N":
+        continue
+    elif choice == "Y":
         break
     else:
-       print("Invalid input")
+        print("Invalid input!! ")
