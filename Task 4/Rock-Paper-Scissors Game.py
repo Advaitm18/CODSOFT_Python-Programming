cb7bb6c8db25ed343d81c937f3396d97a0233a4c
Advait_Mane <advaitm18@gmail.com>
2024-02-21 17:02:07 +0530

    Update Rock-Paper-Scissors Game.py

--- Task 4/Rock-Paper-Scissors Game.py
+++ Task 4/Rock-Paper-Scissors Game.py
@@ -1,79 +1,76 @@
-from random import choice         #In-Built Module:- Random.choice()
-import Display                    #User-Defined Module
-#from termcolor import colored  
-#import termtable
-print('''                              
+from random import choice         # In-Built Module:- Random.choice()
+import Display                    # User-Defined Module 
+import termtables as tt           # pip install termtables
+from termcolor import colored     # pip install termcolor
+
+print('''              _               
          __      _____| | ___ ___  _ __ ___   ___ 
          \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ 
           \ V  V /  __/ | (_| (_) | | | | | |  __/
-           \_/\_/ \___|_|\___\___/|_| |_| |_|\___|                                         
+           \_/\_/ \___|_|\___\___/|_| |_| |_|\___|                                            
 ''')
+
 print("**************************************************************")
 
 user_score = 0
 comp_score = 0
 
+
 while True:
-    user_choice = input(" Enter:-     Rock(R)     Paper(P)     Scissors(S)     Exit(E) \n --->  ").upper()
+    # User Side operation
+    user_choice = input(" Enter:-     Rock(R)     Paper(P)     Scissors(S)     Exit(E) \n --->  ").upper()  
     if user_choice == 'E':
         break
     elif user_choice == 'R':
-        print("Rock")
+        print(colored("User Choice: ", "red", attrs=["bold"]) + "Rock")
         Display.display(user_choice)
     elif user_choice == 'P':
-        print("Paper")
+        print(colored("User Choice: ", "red", attrs=["bold"]) + "Paper")
         Display.display(user_choice)
     elif user_choice == 'S':
-        print("Scissors")
+        print(colored("User Choice: ", "red", attrs=["bold"]) + "Scissors")
         Display.display(user_choice)
     else:
         print("Wrong input <---\n")
         continue
-    
+
+    # Computer/bot side operation
     option = ["R","P","S"]
-    computer_choice = choice(option)
-    print("Computer choose: ")
-    #print(colored('Computer Choose: ','bold'))
+    computer_choice = choice(option)       # Random.choice() function 
+
     if computer_choice == 'R':
-        print("Rock")
+        print(colored("Computer Choice: ", "red", attrs=["bold"]) + "Rock")
         Display.display(computer_choice)
     elif computer_choice == 'P':
-        print("Paper")
+        print(colored("Computer Choice: ", "red", attrs=["bold"]) + "Paper")
         Display.display(computer_choice)
     else:
         computer_choice = 'S'
-        print("Scissors")
+        print(colored("Computer Choice: ", "red", attrs=["bold"]) + "Scissors")
         Display.display(computer_choice)
 
+    # Execution_result 
     if(user_choice == computer_choice):
-        print("Draw")
+        print(colored("Draw! ", "yellow", attrs=["bold"]))
     elif((user_choice == 'R' and computer_choice == 'S') or (user_choice == 'S' and computer_choice == 'P') or (user_choice == 'P' and computer_choice == 'R')):
-        print("You won!")
+        print(colored("You won! ", "yellow", attrs=["bold"]))
         user_score += 1
     else:
-        print("Computer won!")
+        print(colored("Computer won! ", "yellow", attrs=["bold"]))
         comp_score +=1
 
-    print(f'''
-    ---------------------------------
-    | Your Score   | Computer Score |
-    ---------------------------------
-    |  {user_score}|  {comp_score}  |
-    ---------------------------------
-    ''')
-    
-    '''
+    # ScoreBoard_Display 
     my_table=[
         [f"{user_score}" , f"{comp_score}"]
        ]
     headers= ["User_Score"," Computer_Score"]
     tt.print(my_table,header=headers,padding=(0,1),alignment="cc")
-    '''
-        
+
+# Final_Result      
 if(user_score>comp_score):
-    print("\nYou have won!"+"\U0001F603")
+    print(colored("\nYou have won!"+"\U0001F603", "green", attrs=["bold"] ))
 elif(user_score<comp_score):
-    print("\nYou have lost"+", Better Luck Next time! \U0001F605")
+    print(colored("\nYou have lost, "+"Better Luck Next time! \U0001F605", "green", attrs=["bold"]))
 else:
-    print("\nIt is a tie!"+"\U0001F611")
+    print(colored("\nIt is a tie!"+"\U0001F611", "green", attrs=["bold"]))
 
