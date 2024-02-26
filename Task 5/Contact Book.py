8c85edd7dc7ac7d308c526ce7514eb71c008764c
Advait_Mane <advaitm18@gmail.com>
2024-02-26 15:56:16 +0530

    Update Contact Book.py

--- Task 5/Contact Book.py
+++ Task 5/Contact Book.py
@@ -1,218 +1,87 @@
-# importing the required modules  
-import tkinter as tk                    # importing the tkinter module as tk  
-from tkinter import ttk                 # importing the ttk module from the tkinter library  
-from tkinter import messagebox          # importing the messagebox module from the tkinter library  
-import sqlite3 as sql                   # importing the sqlite3 module as sql  
-  
- 
-def add_task():  
-    task_string = task_field.get()  
-    if len(task_string) == 0:  
-        messagebox.showinfo('Error', 'Field is Empty.')  
-    else:   
-        tasks.append(task_string)  
-        the_cursor.execute('insert into tasks values (?)', (task_string ,))  
-        list_update()   
-        task_field.delete(0, 'end')  
-  
-def list_update():  
-    clear_list()  
-    for task in tasks:  
-        task_listbox.insert('end', task)  
-  
-def update_task():
-    message_box = messagebox.askyesno('Delete All', 'Are you sure?')  
-    if message_box == True:   
-        while(len(tasks) != 0):  
-            tasks.pop()  
-        the_cursor.execute('delete from tasks')  
-        list_update()  
-    
-def delete_task():  
-    try:  
-        the_value = task_listbox.get(task_listbox.curselection())  
-        if the_value in tasks:  
-            tasks.remove(the_value)   
-            list_update()   
-            the_cursor.execute('delete from tasks where title = ?', (the_value,))  
-    except:    
-        messagebox.showinfo('Error', 'No Task Selected. Cannot Delete.')        
-   
-def delete_all_tasks():  
-    message_box = messagebox.askyesno('Delete All', 'Are you sure?')  
-    if message_box == True:   
-        while(len(tasks) != 0):  
-            tasks.pop()  
-        the_cursor.execute('delete from tasks')  
-        list_update()  
+import re
+from datetime import datetime
 
-def clear_list():  
-    task_listbox.delete(0, 'end')  
+Contact = []
 
-def view_task():
-    message_box = messagebox.askyesno('Delete All', 'Are you sure?')  
-    if message_box == True:   
-        while(len(tasks) != 0):  
-            tasks.pop()  
-        the_cursor.execute('delete from tasks')  
-        list_update()  
+# To Add the entered data in Contact List
+def add_ContactDetail(name, phno, email, address):
+    Contact.append({"Name":name, "Phone Number":phno, "Email-ID":email, "Address":address})
+    print("Contact Added Successfully!!!")
+    print(Contact)
 
-def close():  
-    print(tasks) 
-    guiWindow.destroy()
+# To Display the data in "Contact_Book.txt" file  
+def list_contact():
+    print(datetime.now().strftime('%B, %d %Y  %H:%M'))
+    for index, contact in enumerate(Contact, start=1):
+        print(f"{index}. {contact['contact']} {contact['phno']} {contact['email']} \n{contact['address']}")
+    print() 
 
-# defining another label using the ttk.Label() widget  
-def task_label(text_name,placex,placey):
-    task_label = ttk.Label(  
-        functions_frame,  
-        text = text_name,  
-        font = ("Times New Roman", "14", "bold"),  
-        background = "#FAEBD7",  
-        foreground = "#000000"  
-    )  
-    task_label.place(x = placex, y = placey)
-    
-def task_field(placex,placey):  
-    task_field = ttk.Entry(  
-        functions_frame,  
-        font = ("Consolas", "12"),  
-        width = 18,  
-        background = "#FFF8DC",  
-        foreground = "#A52A2A"  
-    )  
-    task_field.place(x = placex, y = placey)  
-  
-# function to retrieve data from the database  
-def retrieve_database():  
-    # using the while loop to iterate through the elements in the tasks list  
-    while(len(tasks) != 0):  
-        # using the pop() method to pop out the elements from the list  
-        tasks.pop()  
-    # iterating through the rows in the database table  
-    for row in the_cursor.execute('select title from tasks'):  
-        # using the append() method to insert the titles from the table in the list  
-        tasks.append(row[0])  
-  
-# main function  
-if __name__ == "__main__":  
-    guiWindow = tk.Tk()   
-    guiWindow.title("Contact Book")  
-    guiWindow.geometry("700x550+350+50")  
-    guiWindow.resizable(0, 0)  
-    guiWindow.configure(bg = "#F0F8FF")  
-  
-    # using the connect() method to connect to the database  
-    the_connection = sql.connect('listOfTasks.db')  
-    # creating the cursor object of the cursor class  
-    the_cursor = the_connection.cursor()  
-    # using the execute() method to execute a SQL statement  
-    the_cursor.execute('create table if not exists tasks (title text)')  
-  
-    # defining an empty list  
-    tasks = []  
-      
-    # defining frames using the tk.Frame() widget  
-    header_frame = tk.Frame(guiWindow, bg = "#87CEFA")  
-    functions_frame = tk.Frame(guiWindow, bg = "#87CEFA")  
-    listbox_frame = tk.Frame(guiWindow, bg = "#87CEFA")  
-  
-    # using the pack() method to place the frames in the application  
-    header_frame.pack(fill = "both")  
-    functions_frame.pack(side = "left", expand = True, fill = "both")  
-    listbox_frame.pack(side = "right", expand = True, fill = "both")  
-      
-    # defining a label using the ttk.Label() widget  
-    header_label = ttk.Label(  
-        header_frame,  
-        text = "Contact Book",  
-        font = ("Impact", "40"),  
-        background = "#87CEFA",  
-        foreground = "#002D72"  
-    )  
-    header_label.pack(padx = 20, pady = 20)
+            
+# To valided the entered Phone Number
+def isVaild_phno(phno):     
+    pattern = re.compile("[6-9][0-9]{9}")
+    if re.fullmatch(pattern, phno):
+        print("Vaild Phone Number !!")
+    else:
+        print("Entered Phone No. is Invalid!! ")
+        
+# To valided the entered Emai-ID
+def isVaild_email(email):
+    pattern = re.compile('([A-Za-z0-9]+[._-])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if re.fullmatch(pattern, email):
+        print("Vaild Email-Id !!")
+    else:
+        print(colored("Entered Email-ID is Invaild!! ","red"))
 
-    task_label("Name: ",30,10)
-    task_label("Phone Number: ",30,50)
-    task_label("Email-ID: ",30,90)
-    task_label("Address: ",30,130)
+# To Delete Contact (Option (2))
+def delete_contact(choice):
+    list_contact()
+    if len(Contact) == 0:
+        print("No Contact To Delete ")
+    else:
+        if 0 < choice <= len(Contact):
+            del Contact[choice-1]
+            print("Task Deleted Successfully.")
+        else:
+            print("---> Invaild Contact Number <---")
+            
+# To View Contact_Book (Option (3))
+def view_contact():
+    file = open("Contact_Book.txt")
+    content = file.read()
+    print(content)
+    file.close()
 
-    task_field(160,10)
-    task_field(160,50)
-    task_field(160,90)
-    task_field(160,130)
-  
-    # adding buttons to the application using the ttk.Button() widget  
-    add_button = ttk.Button(  
-        functions_frame,  
-        text = "Add Contact",  
-        width = 20,  
-        command = add_task  
-    )
+while True:
+    print(("CONTACT BOOK").center(50))
+    print("1. Enter the Contact Details 2.Delete the Contact 3.View Contact-Book 4.Exit")
 
-    update_button = ttk.Button(
-        functions_frame,
-        text = "Update Contact",
-        width = 20,
-        command = update_task
-    )
-    
-    del_button = ttk.Button(  
-        functions_frame,  
-        text = "Delete Contact",  
-        width = 20,  
-        command = delete_task  
-    )
-    
-    del_all_button = ttk.Button(  
-        functions_frame,  
-        text = "Delete All Contacts",  
-        width = 20,  
-        command = delete_all_tasks  
-    )
-    
-    view_button = ttk.Button(
-        functions_frame,
-        text = "View Contact",
-        width = 20,
-        command = view_task
-    )
-    
-    exit_button = ttk.Button(  
-        functions_frame,  
-        text = "Exit",  
-        width = 20,  
-        command = close  
-    )
-    
-    # using the place() method to set the position of the buttons in the application  
-    add_button.place(x = 30, y = 200)
-    update_button.place(x = 30, y = 240)
-    del_button.place(x = 30, y = 280)  
-    del_all_button.place(x = 30, y = 320)
-    view_button.place(x = 30, y = 360)
-    exit_button.place(x = 30, y = 400)  
-   
-    task_listbox = tk.Listbox(  
-        listbox_frame,  
-        width = 35,  
-        height = 20,  
-        selectmode = 'SINGLE',  
-        background = "#FFFFFF",  
-        foreground = "#000000",  
-        selectbackground = "#0071c5",  
-        selectforeground = "#FFFFFF"  
-    )  
-    task_listbox.place(x = 15, y = 20)  
-  
-    # calling some functions  
-    retrieve_database()  
-    list_update()  
-    # using the mainloop() method to run the application  
-    guiWindow.mainloop()  
-    # establishing the connection with database  
-    the_connection.commit()  
-    the_cursor.close()  
+    option = int(input("--->  "))
+    if option == 1:
+        name = input("Enter the Name: ")
+        phno = input("Enter the Mobile Number: ")
+        isVaild_phno(phno)
+        email = input("Enter the Email-ID: ")
+        isVaild_email(email)
+        address = input("Enter the Address: ")
 
-
+        add_ContactDetail(name, phno, email, address)
+        
+        #display = f"{('{: <25}'.format(name))} {('{: <15}'.format(phno))} {email} \n{address}"
+        file = open("Contact_Book.txt","a")
+        file.write(list_contact())
+        file.close()
+        
+    elif option == 2:
+        choice = int(input("Enter the Contact Index Number To Delete: "))
+        delete_contact(choice)
+        
+    elif option == 3:
+        view_contact(choice)
+        
+    elif option == 4:
+        break
+    else:
+        print("---> Invaild Input <---")
 
 
