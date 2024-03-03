bd7c7fba266d80fd5b9319882858f69ed4abe403
Advait_Mane <advaitm18@gmail.com>
2024-03-03 12:40:38 +0530

    Update Contact Book.py

--- Task 5/Contact Book.py
+++ Task 5/Contact Book.py
@@ -1,6 +1,6 @@
-import re
-from datetime import datetime
-from termcolor import colored 
+import re, os
+from datetime import datetime    
+from termcolor import colored              # !pip install termcolor
 
 # Empty Contact list to Append entered data 
 Contact = []
@@ -8,29 +8,29 @@ Contact = []
 # This function is used to append entered details in the list(Contact)
 def add_ContactDetail(name, phno, email, address):
     Contact.append({"Name": name, "Phone Number": phno, "Email-ID": email, "Address": address})
-    print(colored("Contact Added Successfully!!!","green"))
+    print(colored("Contact Added Successfully !!!","green"))
 
 # This function used to display the Contact-List
 def list_contact():
     print(colored("CONTACT BOOK","blue",attrs = ["bold"]))
     print(colored("\n" + datetime.now().strftime('%B, %d %Y  %H:%M'),attrs=["bold","blink"]))
     for index, contact in enumerate(Contact, start=1):
-        print(f"\n{index}. {contact['Name']: <30} {contact['Phone Number']: <15} {contact['Email-ID']} \n{contact['Address']: >20}")
+        print(f"\n{index}. {contact['Name']: <30} {contact['Phone Number']: <15} {contact['Email-ID']: <30} {contact['Address']}")
     print()
 
 # This function is used to validate the entered Phone Number 
 def isVaild_phno(phno):
     if re.match("^[6-9][0-9]{9}$", phno):
-        print(colored("Valid Phone Number !!", "green"))
+        print(colored("Valid Phone Number !", "green"))
     else:
-        print(colored("Entered Phone No. is Invalid!!", "red"))
+        print(colored("Entered Phone No. is Invalid !", "red"))
         
 # This function is used to validate the entered Email-ID
 def isVaild_email(email):
     if re.match(r'^([A-Za-z0-9]+[._-])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Za-z]{2,})+$', email):
-        print(colored("Valid Email-Id !!", "green"))
+        print(colored("Valid Email-Id !", "green"))
     else:
-        print(colored("Entered Email-ID is Invalid!!", "red"))
+        print(colored("Entered Email-ID is Invalid !", "red"))
 
 # This function used to delete the entered Contact Details
 def delete_contact(choice):
@@ -39,20 +39,23 @@ def delete_contact(choice):
     else:
         if 0 < choice <= len(Contact):
             del Contact[choice-1]
-            print(colored("Contact Deleted Successfully!!! ","green"))
+            print(colored("Contact Deleted Successfully !!! ","green"))
         else:
             print("---> Invalid Contact Number <---")
 
 # This function used to create .txt file of Contact Details
 def create_contactbook():
-    with open("Contact-Book.txt", "a") as file:
+    print(f"File Path: {os.getcwd()}")
+    print(colored("File Created Successfully !!!", "green"))
+    with open("Contact-Book.txt", "w") as file:
         file.write(datetime.now().strftime('%B, %d %Y  %H:%M'))
         for index, contact in enumerate(Contact, start=1):
-            file.write(f"\n{index}. {contact['Name']: <30} {contact['Phone Number']: <15} {contact['Email-ID']} \n{contact['Address']: >20}")
+            file.write(f"\n{index}. {contact['Name']: <25} {contact['Phone Number']: <15} {contact['Email-ID']: >30}  {contact['Address']}")
         file.write('\n')
 
 while True:
-    print("CONTACT BOOK".center(50))
+    txt = "\nCONTACT BOOK"
+    print(colored(txt.center(50), attrs=["bold"]))
     print("\n1. Enter the Contact Details \n2. Delete the Contact \n3. View Contact-Book \n4. Create Contact-Book File \n5. Exit")
 
     option = int(input("--->  "))
@@ -84,5 +87,3 @@ while True:
 
     else:
         print("---> Invalid Input <---")
-
-
